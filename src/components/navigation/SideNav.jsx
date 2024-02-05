190f44770202690ecd5b36fffe5cbfe920d729ab
thureindev <thureindev@outlook.com>
2024-02-05 23:37:15 +0700

    added navigation components

---
+++ src/components/navigation/SideNav.jsx
@@ -0,0 +1,74 @@
+import { Link } from 'react-router-dom';
+import Slideover from './Slideover';
+import './styles.css';
+import DarkModeToggle from '../dark-mode-toggle/DarkModeToggle';
+
+function joinClassNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+// function setActiveNav(nav, setNav, id) {
+//     // return nav.map((item) => (item.id === id)? {...item, current: true} : {...item, current: false});
+//     return setNav(nav.map((item) => (item.id === id) ? { ...item, current: true } : { ...item, current: false }));
+// }
+
+const SideNav = ({ navigation, isOpen, setIsOpen }) => {
+  return (
+    <>
+      <Slideover
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        header={
+          <div className={`mb-4 ml-4 mr-4`}>
+            <h5
+              className={`mb-2 text-xl font-medium leading-tight filter-none
+                        text-stone-900 dark:text-stone-100`}
+            >
+              thureindev
+            </h5>
+
+            <p className='text-neutral-500 dark:text-neutral-400 filter-none'>
+              Web Developer
+            </p>
+
+            <DarkModeToggle />
+          </div>
+        }
+        content={
+          <ul className={`side-nav ml-4 mr-4`}>
+            {navigation.map((item) => (
+              <li
+                key={item.id}
+                className={joinClassNames(
+                  item.isActive ? 'active' : '',
+                  'side-nav-item'
+                )}
+                onClick={() => {
+                  // setActiveNav(navigation, setNavigation, item.id);
+                  // setNavigation(navigation.map((item) => (item.id === id)? {...item, current: true} : {...item, current: false}));
+                  // setIsOpen(false);
+                  console.log(isOpen);
+                }}
+              >
+                <Link
+                  to={item.href}
+                  className={`side-nav-link mb-1 p-4 ${
+                    item.isActive ? 'active' : ''
+                  }}`}
+                  aria-current={item.isActive ? 'page' : undefined}
+                >
+                  <i>{item.icon}</i>
+                  <span className='pl-4 text-stone-900 dark:text-stone-100'>
+                    {item.name}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        }
+      />
+    </>
+  );
+};
+
+export default SideNav;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 // src/index.js
 export { default as DarkModeToggle } from './components/dark-mode-toggle/DarkModeToggle';
+export { default as updateDocTheme } from './components/dark-mode-toggle/updateDocTheme';
+export { default as Navbar } from './components/navigation/Navbar';
+export { default as SideNav } from './components/navigation/SideNav';
